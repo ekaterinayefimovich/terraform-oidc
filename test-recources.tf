f71a128725bb41063bc1dadef91301108ee2b60d
Kate Yefimovich <ekaterina.yefimovich@innowise.com>
2025-04-30 19:00:20 +0200

    final

--- test-recources.tf
+++ test-recources.tf
@@ -1,31 +1,3 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "super-puper-bucket-test-10"
-}
-
-resource "aws_s3_bucket" "s3_bucket_1" {
-  bucket = "super-puper-bucket-test-10-1"
-}
-
-resource "aws_iam_role" "test_role" {
-  name = "test_role_for_pipeline_10"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-
-  tags = {
-    tag-key = "tag-value"
-  }
+  bucket = "super-puper-bucket-test-11"
 }
