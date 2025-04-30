d06353e74efff730eac610ad14deea71e673010a
Kate Yefimovich <ekaterina.yefimovich@innowise.com>
2025-04-30 18:54:20 +0200

    the end???

--- test-recources.tf
+++ test-recources.tf
@@ -1,13 +1,13 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "super-puper-bucket-test-9"
+  bucket = "super-puper-bucket-test-10"
 }
 
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "super-puper-bucket-test-9-1"
+resource "aws_s3_bucket" "s3_bucket_1" {
+  bucket = "super-puper-bucket-test-10-1"
 }
 
 resource "aws_iam_role" "test_role" {
-  name = "test_role_for_pipeline_9"
+  name = "test_role_for_pipeline_10"
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
