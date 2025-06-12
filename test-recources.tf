0b1005a894a98df74dc15a317e0c997c00c87e1c
Katya Yefimovich <ekaterina.yefimovich@innowise.com>
2025-06-12 19:52:26 +0300

    test commit 2

--- test-recources.tf
+++ test-recources.tf
@@ -1,6 +1,3 @@
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "super-puper-bucket-test-555"
-}
-resource "aws_s3_bucket" "s3_bucket_2" {
-  bucket = "super-puper-bucket-test-55"
+resource "aws_s3_bucket" "s3_bucket_" {
+  bucket = "super-puper-bucket-test-65"
 }
