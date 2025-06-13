e60dcfb5f7f04e329dc67e75a9055591722c2366
Katya Yefimovich <ekaterina.yefimovich@innowise.com>
2025-06-13 12:56:33 +0300

    new branch commit 1

--- test-recources.tf
+++ test-recources.tf
@@ -2,5 +2,5 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket = "super-puper-bucket-test-54"
 }
 resource "aws_s3_bucket" "s3_bucket_1" {
-  bucket = "super-puper-bucket-test-66"
+  bucket = "super-puper-bucket-test-6"
 }
